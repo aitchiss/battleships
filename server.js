cdab700cd6865e59d418d74167469b759dd83901
Suzanne <suzanne.aitchison@gmail.com>
2017-05-08 19:38:21 +0100

    server file set up

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
+
+app.use(express.static('client/build'))
+
+
+http.listen(3000, function (){
+  console.log('app listening at 3000')
+})
