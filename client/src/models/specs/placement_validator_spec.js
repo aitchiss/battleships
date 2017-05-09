35de47303193ab67f38c1cbfb2ab38924594aab9
Suzanne <suzanne.aitchison@gmail.com>
2017-05-09 20:50:53 +0100

    validator returns false if wrong size of ship submitted

--- client/src/models/specs/placement_validator_spec.js
+++ client/src/models/specs/placement_validator_spec.js
@@ -7,7 +7,11 @@ describe('PlacementValidator tests:', function(){
 
   it('validates true for ship of size 1, expected size 1', function(){
     assert.strictEqual(true, placementValidator.validate(1, [[0, 0]]))
+  })
 
+  it('returns false for ship of wrong length', function(){
+    var coords = [[1, 0], [2, 1]]
+    assert.strictEqual(false, placementValidator.validate(1, coords))
   })
 
   })
