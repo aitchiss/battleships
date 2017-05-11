eb73321edd8c7516bdc6ee11bd66d64605b116dc
Suzanne <suzanne.aitchison@gmail.com>
2017-05-11 21:03:24 +0100

    altered sh file

--- start.sh
+++ start.sh
@@ -6,6 +6,10 @@ echo "installing npm modules in client..."
 cd "client"
 npm install
 
+echo "starting server..."
+cd ".."
+npm start
+
 echo "opening player 1 and player 2 browser windows..."
 
 open http://localhost:3000
