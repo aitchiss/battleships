971f07b99f19c31a96b262a6b5bbd17f29fdee5c
Suzanne <suzanne.aitchison@gmail.com>
2017-05-11 21:00:15 +0100

    adds sh file

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "installing npm modules in top level..."
+npm install
+
+echo "installing npm modules in client..."
+cd "client"
+npm install
+
+echo "opening player 1 and player 2 browser windows..."
+
+open http://localhost:3000
+open http://localhost:3000
+
+echo "finished"
